34b624246244ba68f8b7375bde6e7c00d5a433c6
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-11-21 10:29:28 -0500

    setup smoke tests project

---
+++ smoke-tests/tests/augmentation/file-test.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+const execa = require('execa');
+const { assert } = require('chai');
+
+describe('app-augmentation', () => {
+  it('did a thing', () => {
+    assert.ok(true);
+  });
+});
